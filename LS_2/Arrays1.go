21c723bec245b840ee9e0fa323bd2f8241831e32
andreasheige <a.heige.83@gmail.com>
2021-08-01 22:13:39 +0200

    Printing array with Printf

--- LS_2/Arrays1.go
+++ LS_2/Arrays1.go
@@ -10,6 +10,9 @@ func main() {
 	a[1] = "Horld"
 	fmt.Println(a[0], a[1]) // will print both arrays
 	fmt.Println(a)          // will print both arrays as one
+	fmt.Printf("%s\n", a)
+
+	fmt.Printf("%q\n", a)
 
 	primes := [6]int{2, 3, 5, 7, 11, 13} // array of prime numbers of size 6
 	fmt.Println(primes)
