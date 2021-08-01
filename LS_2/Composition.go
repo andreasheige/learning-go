bcf0b6382e13a94d49b6168667a769ed5ce51c17
andreasheige <a.heige.83@gmail.com>
2021-08-01 21:32:33 +0200

    use a struct literal

--- LS_2/Composition.go
+++ LS_2/Composition.go
@@ -15,12 +15,24 @@ type Player struct {
 	GameId int
 }
 
+// func main() {
+// 	p := Player{}
+// 	p.Id = 13
+// 	p.Name = "Andreas"
+// 	p.Location = "GBG"
+// 	p.GameId = 41307
+// 	fmt.Printf("%+v", p) // the value in a default format when printing structs,
+// 	// the plus flag (%+v) adds field names
+// }
 func main() {
-	p := Player{}
-	p.Id = 13
-	p.Name = "Andreas"
-	p.Location = "GBG"
-	p.GameId = 41307
-	fmt.Printf("%+v", p) // the value in a default format when printing structs,
-	// the plus flag (%+v) adds field names
+	p := Player{
+		User{Id: 13, Name: "Andreas", Location: "GBG"},
+		90404,
+	}
+	fmt.Printf(
+		"Id: %d, Name: %s, Location: %s, Game id: %d\n",
+		p.Id, p.Name, p.Location, p.GameId)
+	// Directly set a field defined on the Player struct
+	p.Id = 11
+	fmt.Printf("%+v", p)
 }
