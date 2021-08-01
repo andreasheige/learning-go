c944f063f22524396cbc8d50db1ccd5a7f81f3d9
andreasheige <a.heige.83@gmail.com>
2021-08-01 21:35:57 +0200

    added Greetings

--- LS_2/Composition.go
+++ LS_2/Composition.go
@@ -9,30 +9,36 @@ type User struct {
 	Name, Location string
 }
 
+func (u *User) Greetings() string {
+	return fmt.Sprintf("Hi %s from %s", u.Name, u.Location)
+}
+
 //type Player with one additional attribute
 type Player struct {
 	User   //user will contain all the required attributes
 	GameId int
 }
 
-// func main() {
-// 	p := Player{}
-// 	p.Id = 13
-// 	p.Name = "Andreas"
-// 	p.Location = "GBG"
-// 	p.GameId = 41307
-// 	fmt.Printf("%+v", p) // the value in a default format when printing structs,
-// 	// the plus flag (%+v) adds field names
-// }
 func main() {
-	p := Player{
-		User{Id: 13, Name: "Andreas", Location: "GBG"},
-		90404,
-	}
-	fmt.Printf(
-		"Id: %d, Name: %s, Location: %s, Game id: %d\n",
-		p.Id, p.Name, p.Location, p.GameId)
-	// Directly set a field defined on the Player struct
-	p.Id = 11
-	fmt.Printf("%+v", p)
+	p := Player{}
+	p.Id = 13
+	p.Name = "Andreas"
+	p.Location = "GBG"
+	p.GameId = 41307
+	fmt.Println(p.Greetings())
+	//fmt.Printf("%+v", p) // the value in a default format when printing structs,
+	// the plus flag (%+v) adds field names
 }
+
+// func main() {
+// 	p := Player{
+// 		User{Id: 13, Name: "Andreas", Location: "GBG"},
+// 		90404,
+// 	}
+// 	fmt.Printf(
+// 		"Id: %d, Name: %s, Location: %s, Game id: %d\n",
+// 		p.Id, p.Name, p.Location, p.GameId)
+// 	// Directly set a field defined on the Player struct
+// 	p.Id = 11
+// 	fmt.Printf("%+v", p)
+// }
